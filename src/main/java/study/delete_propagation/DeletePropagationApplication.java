1a6249cded706816aa75379781869f40f16ac8a3
1sunJ <won9619v@naver.com>
2024-11-15 18:54:31 +0900

    Feat : 엔티티 세팅

---
+++ src/main/java/study/delete_propagation/DeletePropagationApplication.java
@@ -0,0 +1,13 @@
+package study.delete_propagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeletePropagationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeletePropagationApplication.class, args);
+	}
+
+}
